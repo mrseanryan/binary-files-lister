47fb1e2422f54f59f51c4cb749e90b9682100576
Sean Ryan <sean.ryan@mendix.com>
2021-08-18 17:48:03 +0200

    Add options to filter and to show dependencies

--- list.ps1
+++ list.ps1
@@ -1,4 +1,46 @@
-param([string][Parameter(Mandatory = $True)] [ValidateNotNull()] $dirPath)
+param(
+    [string] $dirPath,
+    [string] $option1,
+    [string] $option2
+)
+
+if (-not $dirPath) {
+    Write-Output("USAGE: list.ps1 <path to directory> [-dependencies] [-filter=MyText]")
+    Write-Output("  OPTIONS:")
+    Write-Output("  -dependencies - Also show the .NET dependencies")
+    Write-Output("  -filter - Filter to filenames that contain the given text")
+    Exit
+}
+
+function ProcessFilterByNameOption($value) {
+    if ($value.StartsWith("filter=")) {
+        return  $value.Split("=")[1]
+    }
+    return $null
+}
+
+function ProcessDependenciesOption($value) {
+    Write-Verbose("'$value'") # TODO why is this needed?
+    $test = $value -eq "dependencies"
+    return $test
+}
+
+$doDependencies = ProcessDependenciesOption($option1)
+if (-not $doDependencies) {
+    $doDependencies = ProcessDependenciesOption($option2)
+}
+
+$filterByName = ProcessFilterByNameOption($option1)
+if (-not $filterByName) {
+    $filterByName = ProcessFilterByNameOption($option2)
+}
+
+if ($doDependencies) {
+    Write-Verbose("Also outputting dependencies")
+}
+if ($filterByName) {
+    Write-Verbose("Filtering to filenames like '$filterByName'")
+}
 
 Write-Output("File name, Size in Kb, Version")
 
@@ -13,18 +55,18 @@ function Write-File-Details {
     Write-Output($file.name + ", " + $sizeInKb + ", " + $version)
 }
 
-# xxx option -filter "MyDllPrefix"
-foreach ($file in (Get-ChildItem $dirPath | Where-Object { $_.extension -in ".dll", ".exe" } )) {
+foreach ($file in (Get-ChildItem $dirPath | Where-Object { $_.extension -in (".dll", ".exe") -and (($null -eq $filterByName) -or ($_.Name.Contains($filterByName))) } )) {
     Write-File-Details($file)
 
-    # xxx make option -dependencies
-    try {
-        $loadedNetAssembly = [System.Reflection.Assembly]::LoadFrom($file.FullName)
-        foreach ($asm in $loadedNetAssembly.GetReferencedAssemblies()) {
-            Write-Output(" -> $asm") # note: outputting detais of dependencies is not easy (location can be in GAC...)
+    if ($doDependencies) {
+        try {
+            $loadedNetAssembly = [System.Reflection.Assembly]::LoadFrom($file.FullName)
+            foreach ($asm in $loadedNetAssembly.GetReferencedAssemblies()) {
+                Write-Output(" -> $asm") # note: outputting detais of dependencies is not easy (location can be in GAC...)
+            }
         }
-    }
-    catch [BadImageFormatException] {
-        # Is not a .NET assembly
+        catch [BadImageFormatException] {
+            # Is not a .NET assembly
+        }
     }
 }
