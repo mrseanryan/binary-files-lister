6f5638b42cbfe326cd32cec0c68100be6c3a214b
Sean Ryan <sean.ryan@mendix.com>
2021-08-18 16:45:00 +0200

    Tidy code

--- list.ps1
+++ list.ps1
@@ -1,10 +1,5 @@
 param([string][Parameter(Mandatory=$True)] [ValidateNotNull()] $dirPath)
 
-# TODO
-#
-# Directory-/File-name      File Size (KB)       Origin    OSS/Commercial/Internally Developed
-# modeler/Mendix.CommandLine.Shared.dll           16           NL/Neutral/US           Internally Developed
-
 Write-Output("File name, Size in Kb, Version")
 
 foreach ($file in (Get-ChildItem $dirPath | Where-Object {$_.extension -in ".dll", ".exe"} )) {
